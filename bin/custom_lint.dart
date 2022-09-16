155eb1251d6c4bf41470cc00897490907349d75b
Kishan Singh <kishansr@sostronk.com>
2022-09-16 15:25:02 +0530

    Add lints file in lib folder

--- bin/custom_lint.dart
+++ bin/custom_lint.dart
@@ -1,11 +1,7 @@
 import 'dart:isolate';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'circular_progress_indicator_lint.dart';
-import 'debug_print_lint.dart';
-import 'gesture_detector_lint.dart';
-import 'riverpod_final_lint.dart';
-import 'set_state_lint.dart';
+import 'package:sostronk_k2_lints/sostronk_k2_lints.dart';
 
 void main(List<String> args, SendPort sendPort) {
   startPlugin(sendPort, _IndexPlugin());
---
+++ lib/sostronk_k2_lints.dart
@@ -0,0 +1,7 @@
+library sostronk_k2_lints;
+
+export 'src/circular_progress_indicator_lint.dart';
+export 'src/debug_print_lint.dart';
+export 'src/gesture_detector_lint.dart';
+export 'src/riverpod_final_lint.dart';
+export 'src/set_state_lint.dart';
